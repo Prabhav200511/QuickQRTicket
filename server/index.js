a2a74dc4f226420d0a860b41b47483149eecc74f
Prabhav200511 <manikwarkrishnaprabhav@gmail.com>
2025-07-27 01:21:25 +0530

    Proj phase1 done 1

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const eventRoutes = require('./routes/eventRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
+const path = require('path');
 
+
+const clientBuildPath = path.join(__dirname, '../client/dist');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,6 +22,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(clientBuildPath));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
@@ -33,4 +37,8 @@ app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
