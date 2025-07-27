f076a5fcdc10ccd7cd7ea02223e3e3d0dde0410c
Prabhav200511 <manikwarkrishnaprabhav@gmail.com>
2025-07-27 21:40:20 +0530

    Proj phase1 done 11

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, useMemo } from 'react';
+import  { createContext, useContext, useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 
 axios.defaults.baseURL =  import.meta.env.NODE_ENV==="production"? '/' : 'http://localhost:5000';
