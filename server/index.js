0bd082afea2cb321c18e03e1c3d28409f9778c26
Prabhav200511 <manikwarkrishnaprabhav@gmail.com>
2025-08-01 09:19:00 +0530

    Proj phase1 done 26

--- server/index.js
+++ server/index.js
@@ -8,16 +8,15 @@ const eventRoutes = require('./routes/eventRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
 const path = require('path');
 
-__dirname = path.resolve();
-const clientBuildPath = path.join(__dirname, '../client/dist');
+// __dirname = path.resolve();
+// const clientBuildPath = path.join(__dirname, '../client/dist');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: [
-    'http://localhost:5173',               // local dev frontend
-    'https://quickqrticket.onrender.com'   // deployed frontend
+    process.env.FRONTEND_URL
   ],
   credentials: true,
 };
@@ -26,7 +25,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(clientBuildPath));
+//app.use(express.static(clientBuildPath));
 
 app.use('/api/auth', authRoutes); 
 app.use('/api/events', eventRoutes);
@@ -37,9 +36,9 @@ app.get('/api', (req, res) => {
   res.status(200).json({ message: 'QuickTicket API is running!' });
 });
 
-app.get('/{*any}', (req, res) => {
-  res.sendFile(path.join(clientBuildPath, 'index.html'));
-});
+// app.get('/{*any}', (req, res) => {
+//   res.sendFile(path.join(clientBuildPath, 'index.html'));
+// });
 
 app.use(errorHandler);
 
